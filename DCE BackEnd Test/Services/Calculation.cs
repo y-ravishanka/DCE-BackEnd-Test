9f32363107ff2ede173fe6d8fb50596f3f79e128
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 16:24:28 +0530

    creation of bool convertion methods

--- DCE BackEnd Test/Services/Calculation.cs
+++ DCE BackEnd Test/Services/Calculation.cs
@@ -2,5 +2,31 @@
 {
     internal class Calculation : ICalculation
     {
+        int ICalculation.BoolToIntConvert(bool t)
+        {
+            switch (t)
+            {
+                case true :
+                    return 1;
+                    
+                case false :
+                    return 0;
+            }
+        }
+
+        bool ICalculation.IntToBoolConvert(int t)
+        {
+            switch (t)
+            {
+                case 1:
+                    return true;
+                
+                case 0:
+                    return false;
+                
+                default :
+                    return false;
+            }
+        }
     }
 }
