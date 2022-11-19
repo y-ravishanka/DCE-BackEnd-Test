39bca76f5dae742954014a0036ccdfd81ed91adc
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 17:33:32 +0530

    declare get order, product and supplier details methods

--- DCE BackEnd Test/Services/IDatabase.cs
+++ DCE BackEnd Test/Services/IDatabase.cs
@@ -6,11 +6,20 @@ namespace DCE_BackEnd_Test.Services
     {
         bool CheckConnection();
 
-        #region  Customer Section
+        #region Customer Section
         bool InsertCustomer(Customer cus);
         List<Customer> GetCustomers();
         bool UpdateCustomer(Customer cus);
         bool DeleteCustomer(string id);
+        List<Order> ActiveOrdersByCustomer(string id);
+        #endregion
+
+        #region Product Section
+        Product GetProductById(string id);
+        #endregion
+
+        #region Supplier Section
+        Supplier GetSupplierById(string id);
         #endregion
     }
 }
