f865f1eeaf173ca91e6561a27ffd6ca2d665c695
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 15:36:37 +0530

    create customer model

--- DCE BackEnd Test/Models/Customer.cs
+++ DCE BackEnd Test/Models/Customer.cs
@@ -2,5 +2,12 @@
 {
     public class Customer
     {
+        public string? UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
     }
 }
