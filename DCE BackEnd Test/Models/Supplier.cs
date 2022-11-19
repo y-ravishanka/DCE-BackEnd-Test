babfc12f9340396f89787176d1db5ec02045324c
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 17:12:49 +0530

    create supplier model

---
+++ DCE BackEnd Test/Models/Supplier.cs
@@ -0,0 +1,10 @@
+namespace DCE_BackEnd_Test.Models
+{
+    public class Supplier
+    {
+        public string? SupplierId { get; set; }
+        public string? SupplierName { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
