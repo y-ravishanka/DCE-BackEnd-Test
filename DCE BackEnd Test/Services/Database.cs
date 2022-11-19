e14d38eb947a0d20e3155a2f435a6d4a89f648e4
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 16:10:27 +0530

    create insert customer method

--- DCE BackEnd Test/Services/Database.cs
+++ DCE BackEnd Test/Services/Database.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using DCE_BackEnd_Test.Models;
 
 namespace DCE_BackEnd_Test.Services
 {
@@ -34,9 +35,50 @@ namespace DCE_BackEnd_Test.Services
     
         #region Customer Section
         // a method to insert a customer data to the database
+        bool IDatabase.InsertCustomer(Customer cus)
+        {
+            bool response = false;
+            int IsActive = 0;
+            que = "insert into(UserId,Username,Email,FirstName,LastName,CreatedOn,IsActive) values( default,'" + cus.Username + "','" + cus.Email + "','" + cus.FirstName + "','" + cus.LastName + "','" + cus.CreatedOn + "'," + IsActive + ")";
+            SqlCommand cmd = new(que, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                response = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                response = false;
+            }
+            finally
+            { con.Close(); }
+            GC.Collect();
+            return response;
+        }
+
         // a method to get data of all customers from the database
+        List<Customer> IDatabase.GetCustomers()
+        {
+            List<Customer> list = new();
+            return list;
+        }
+
         // a method to update a customer data of the database
+        bool IDatabase.UpdateCustomer(string id)
+        {
+            bool response = false;
+            return response;
+        }
+
         // a method to delete a customer from the database
+        bool IDatabase.DeleteCustomer(string id)
+        {
+            bool response = false;
+            return response;
+        }
+
         #endregion
 
         #region Supplier Section
