2725bdb05728b87306518d4990335bce0468fa16
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 16:09:55 +0530

    declaring Customer database interaction methods

--- DCE BackEnd Test/Services/IDatabase.cs
+++ DCE BackEnd Test/Services/IDatabase.cs
@@ -1,7 +1,16 @@
-﻿namespace DCE_BackEnd_Test.Services
+﻿using DCE_BackEnd_Test.Models;
+
+namespace DCE_BackEnd_Test.Services
 {
     internal interface IDatabase
     {
         bool CheckConnection();
+
+        #region  Customer Section
+        bool InsertCustomer(Customer cus);
+        List<Customer> GetCustomers();
+        bool UpdateCustomer(string id);
+        bool DeleteCustomer(string id);
+        #endregion
     }
 }
