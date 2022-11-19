661a2f8b584518b62fc8c413ee9d88f36f27c78b
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 13:57:28 +0530

    add comments to the database connection class

--- DCE BackEnd Test/Services/Database.cs
+++ DCE BackEnd Test/Services/Database.cs
@@ -7,7 +7,7 @@ namespace DCE_BackEnd_Test.Services
         private readonly SqlConnection con = new(@"Data Source=DESKTOP-1G3ODQ5;Initial Catalog=dceBackEndTest;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
         private string? que = null;
 
-        #region Check Database Connection
+        #region Check Database Connection Section
         bool IDatabase.CheckConnection()
         {
             bool test = false;
@@ -30,6 +30,19 @@ namespace DCE_BackEnd_Test.Services
             GC.Collect();
             return test;
         }
+        #endregion
+    
+        #region Customer Section
+        // a method to insert a customer data to the database
+        // a method to get data of all customers from the database
+        // a method to update a customer data of the database
+        // a method to delete a customer from the database
+        #endregion
+
+        #region Supplier Section
+        #endregion
+
+        #region  Product Section
         #endregion
     }
 }
