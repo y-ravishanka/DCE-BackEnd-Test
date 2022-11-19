9c055f0e3bf368481323503ef9791ad58bfa6523
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-19 13:45:03 +0530

    add Product table to the script

--- Database/DCE BackEnd Test Database.sql
+++ Database/DCE BackEnd Test Database.sql
@@ -17,9 +17,9 @@ CREATE TABLE Customer
 );
 
 /* basic Customer table function queries */
-SELECT * FROM Customer
-DELETE Customer
-DROP TABLE Customer
+SELECT * FROM Customer;
+DELETE Customer;
+DROP TABLE Customer;
 
 /* Order table sql script */
 CREATE TABLE [Order]
@@ -33,12 +33,29 @@ CREATE TABLE [Order]
 	ShippedOn DATETIME NOT NULL,
 	IsActive BIT DEFAULT(1),
 	PRIMARY KEY (OrderId)
-)
+);
 
 /* basic Order table function queries */
-SELECT * FROM [Order]
-DELETE [Order]
-DROP TABLE [Order]
+SELECT * FROM [Order];
+DELETE [Order];
+DROP TABLE [Order];
+
+/* Product table sql script */
+CREATE TABLE Product
+(
+	ProductId UNIQUEIDENTIFIER DEFAULT NEWID(),
+	ProductName VARCHAR(50) NOT NULL,
+	UnitPrice DECIMAL NOT NULL,
+	SupplierId UNIQUEIDENTIFIER NOT NULL,
+	CreatedOn DATETIME NOT NULL,
+	IsActive BIT DEFAULT(1),
+	PRIMARY KEY (ProductId)
+)
+
+/* basic Product table function queries */
+SELECT * FROM Product
+DELETE Product
+DROP TABLE Product
 
 /* Supplier table sql script */
 CREATE TABLE Supplier
@@ -47,11 +64,11 @@ CREATE TABLE Supplier
 	SupplierName VARCHAR(50) NOT NULL,
 	CreatedOn DATETIME NOT NULL,
 	IsActive BIT DEFAULT(1)
-)
+);
 
 /* basic Supplier table function queries */
-SELECT * FROM Supplier
-DELETE Supplier
-DROP TABLE Supplier
+SELECT * FROM Supplier;
+DELETE Supplier;
+DROP TABLE Supplier;
 
 
